3bb28eb140016cab53b1e9b591135805e54899e4
Parth Italiya <133667675+italiyaparth@users.noreply.github.com>
2025-01-28 15:11:30 +0530

    Update learnings

---
+++ learnings/test.cmd
@@ -0,0 +1,12 @@
+Source : 
+
+Custom1 : 
+
+	Table.TransformColumns(
+		Source, {"Content", each Excel.Workbook(_, true)}
+	)
+
+Expanded Content :
+
+Data : 
+	Table.Combine(#"Expanded Content"[Data])
